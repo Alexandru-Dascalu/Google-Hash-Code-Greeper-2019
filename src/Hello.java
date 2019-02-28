92f49554b1f9d54cfa60aba45b7dc81004dd1c1a
Ivan <iivvaann.andreev@gmail.com>
2019-02-28 18:08:24 +0000

    hash

--- src/Hello.java
+++ src/Hello.java
@@ -1,13 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
+        ArrayList<Photo> photos = new ArrayList<>();
+
         // reader
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
-        for(int i=0; i<n; i++){
-            String tags = in.nextLine();
+        for (int i = 0; i < n; i++) {
+            String readTags = in.nextLine();
+            String[] split = readTags.split(" ");
+
+            String orientation = split[0];
+
+            // dont need the second one
+
+            ArrayList<String> tags = new ArrayList<>();
+            for (int j = 2; j < split.length - 2; j++) {
+                tags.add(split[j]);
+            }
+
+            photos.add(new Photo(orientation == "H" ? Photo.Orientation.H : Photo.Orientation.V,
+                    tags));
         }
 
     }

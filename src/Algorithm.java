d8eeef41f056667b414a5ff40b6602d18a4b4c2d
Bassam Helal <basshelal@gmail.com>
2019-02-28 18:47:41 +0000

    stream

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,8 +1,10 @@
+import javax.swing.text.html.HTML;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Algorithm {
 
-    public static int interestScore(Slide s1, Slide s2){
+    public static int interestScore(Slide s1, Slide s2) {
 
         ArrayList<String> allTags = new ArrayList<>();
 
@@ -13,27 +15,34 @@ public class Algorithm {
         ArrayList<String> s2NOTs1 = new ArrayList<>();
 
         //Populating allTags
-        for (Photo pic : s1.photos){
+        for (Photo pic : s1.photos) {
             allTags.addAll(pic.tags);
         }
-        for (Photo pic : s2.photos){
+        for (Photo pic : s2.photos) {
             allTags.addAll(pic.tags);
         }
 
         //Populating commonTag
-        for (String tag : allTags){
+        for (String tag : allTags) {
             commonTags.add(tag);
         }
 
 
-        for (Photo pic : s1.photos){
+        for (Photo pic : s1.photos) {
 
         }
+
+        s1NOTs2.addAll(
+                allTags.stream().filter(tag -> s1.getTags().contains(tag) && !s2.getTags().contains(tag)
+                ).collect(Collectors.toList()));
+
+        s2NOTs1.addAll(allTags.stream().filter(tag -> !s1.getTags().contains(tag) && s2.getTags().contains(tag)
+        ).collect(Collectors.toList()));
 
 
 //        allTags.stream().filter(tag -> )
 
-        return 0 ;
+        return 0;
     }
 
 }
